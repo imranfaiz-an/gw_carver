7fa310403bd9f51c1c2e4d7a69302b8a90738b45
Apollinaire <eabi@uchicago.edu>
2025-11-09 11:49:08 -0600

    edited Sarah's R code to account for path changes

--- code/data_cleaning_IR_report_card.R
+++ code/data_cleaning_IR_report_card.R
@@ -3,14 +3,14 @@ library(tidyverse)
 library(writexl)
 
 setwd("./data_sources")
-math_prof <- read_excel("2025-Report-Card-Public-Data-Set.xlsx", sheet = "ELAMathScience")
+math_prof <- read_excel("raw_data/2025-Report-Card-Public-Data-Set.xlsx", sheet = "ELAMathScience")
 colnames(math_prof)
 
 math_prof <- math_prof %>% 
   filter(City == "Chicago", Level == "School") %>%
   select(RCDTS, `School Name`, City, `% ELA Proficiency`, `% Math Proficiency`)
 
-science_prof <- read_excel("2025-Report-Card-Public-Data-Set.xlsx", sheet = "ELAMathScience (2)")
+science_prof <- read_excel("raw_data/2025-Report-Card-Public-Data-Set.xlsx", sheet = "ELAMathScience (2)")
 colnames(science_prof)
 science_prof <- science_prof %>%
   filter(City == "Chicago", Level == "School") %>%
@@ -18,4 +18,4 @@ science_prof <- science_prof %>%
 
 math_and_science_prof <- left_join(math_prof,science_prof, by= c("RCDTS","School Name", "City"))
 
-write_xlsx(math_and_science_prof, "report_card_proficiency_scores.xlsx")
+write_xlsx(math_and_science_prof, "output/report_card_proficiency_scores.xlsx")
